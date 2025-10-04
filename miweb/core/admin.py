652158b368828add2aa3a9f65a647fa07ba0bd67
Team-FDF <fa.troncoso@duocuc.cl>
2025-10-04 15:14:39 -0300

    Delete miweb directory

--- miweb/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import cotizador
-
-@admin.register(cotizador)
-class cotizadorAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'email', 'mensaje', 'fecha')  
-    search_fields = ('nombre', 'email')
